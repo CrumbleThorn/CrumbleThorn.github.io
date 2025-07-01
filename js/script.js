2945109e847e172d535dcfc1ab52dbb6f6f61d49
CrumbleThorn <jctamayo5@alum.up.edu.ph>
2025-07-01 19:50:55 +1000

    Added TitleBar Class and moved individual UI Handlers to their own functions

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 import * as animate from './modules/animate.js';
 import * as anim from './modules/animations.js';
+import * as templates from './modules/templates.js';
 import * as ui from './modules/ui.js';
 import * as util from './modules/util.js';
 
@@ -13,85 +14,168 @@ const pageClasses = {
     endSection: 'end-section'
 }
 const pageElements =  {
-    // Global Screen Elements
-    main: 'main',
+    titleBar: 'title-bar',
+    titleBarContent: 'title-bar-content',
+    titleText: 'title-text',
+    start: 'start',
 }
-        
+
+// Temporary variables while definitions are not put into classes
+let hero;
+let heroContent;
+let titleBar;
+
+class TitleBar {
+    constructor(obj) {
+        this.obj = obj;
+        this.titleContent = document.getElementById(pageElements.titleBarContent);
+        this.titleText = document.getElementById(pageElements.titleText);
+        this.titleTrigger = new anim.AnimationScrollTrigger(heroContent,
+                                                            anim.animationType.hide,
+                                                            new anim.ScrollTriggerElement(this.obj),
+                                                            undefined,
+                                                            undefined,
+                                                            0,
+                                                            undefined,
+                                                            true);
+        this.heroTrigger = new anim.AnimationScrollTrigger(heroContent,
+                                                            anim.animationType.show,
+                                                            new anim.ScrollTriggerElement(hero.elem.obj, anim.anchor.bottom),
+                                                            new anim.ScrollTriggerElement(window),
+                                                            anim.scrollTriggerType.onScrollUp,
+                                                            0,
+                                                            undefined,
+                                                            true);
+        window.addEventListener('scrollTriggered', (event) => {this.handle(event)});
+    }
+
+    handle(event) {
+        if (event.detail.origin == this.obj && event.detail.animatedElement == heroContent.obj) {
+            window.scrollTo(0, this.obj.getBoundingClientRect().top + 5);
+        } else if (event.detail.origin == hero.elem.obj && event.detail.animatedElement == heroContent.obj) {
+            util.scrollToTop();
+        }
+    }
+
+    modifyText(newText) {
+        // TO DO: Logic
+        this.titleText.innerHTML = newText;
+    }
+}
+
+// TO DO: Move to Hero UI Class
+function handleHero() {
+    if (Object.hasOwn(loadedDOMS, templates.templateList.heroTemplate)) {
+        util.log('Hero Detected!');
+        hero = new ui.Hero(new anim.AnimatedElement(loadedDOMS.hero[0].shadow.getElementById(util.css.siteElements.hero)));
+        const heroButton = new anim.AnimatedElement(loadedDOMS.hero[0].shadow.getElementById('hero-button'),
+                                                    false,
+                                                    new animate.Animation(animate.animationClass.fadeInUp,
+                                                                            animate.speedClass.fast,
+                                                                            1000
+                                                                            ));
+        const heroMenuText = new anim.AnimatedElement(loadedDOMS.hero[0].shadow.getElementById('hero-menu-text'),
+                                                        false,
+                                                        new animate.Animation(animate.animationClass.fadeInUp,
+                                                                                animate.speedClass.fast,
+                                                                                1200
+                                                                                ));
+        const scrollDownText = new anim.AnimatedElement(loadedDOMS.hero[0].shadow.getElementById('scroll-down-text'),
+                                                        false,
+                                                        new animate.Animation(animate.animationClass.fadeInUp,
+                                                                                animate.speedClass.fast,
+                                                                                1400
+                                                                                ),
+                                                        undefined,
+                                                        new animate.Animation(animate.animationClass.bounce,
+                                                                                animate.speedClass.slow,
+                                                                                animate.delayClass.delay_4s,
+                                                                                animate.repeatClass.infinite
+                                                                                ));
+        const heroTitle = new anim.AnimatedElement(loadedDOMS.hero[0].shadow.getElementById('hero-title'),
+                                                    false,
+                                                    new animate.Animation(animate.animationClass.backInLeft,
+                                                                            animate.speedClass.animated,
+                                                                            ));
+        const heroSubtitle = new anim.AnimatedElement(loadedDOMS.hero[0].shadow.getElementById('hero-subtitle'),
+                                                        false,
+                                                        new animate.Animation(animate.animationClass.backInLeft,
+                                                                                animate.speedClass.animated,
+                                                                                500
+                                                                                ));
+        const heroImage = new anim.AnimatedElement(loadedDOMS.hero[0].shadow.getElementById('hero-image'),
+                                                    false,
+                                                    new animate.Animation(animate.animationClass.backInRight,
+                                                                            animate.speedClass.animated,
+                                                                            200
+                                                                            ));
+        heroContent = new anim.AnimatedElement(loadedDOMS.hero[0].shadow.getElementById(util.css.siteElements.heroContent),
+                                                        false,
+                                                        new animate.Animation(animate.animationClass.fadeInLeft,
+                                                                                animate.speedClass.animated,
+                                                                                ),
+                                                        new animate.Animation(animate.animationClass.fadeOutUp,
+                                                                                animate.speedClass.animated));
+        // TO DO: Create AnimationEventTrigger for these
+        hero.elem.obj.addEventListener('showAnimationComplete', (event) => {
+            if (event.target == heroContent.obj) {
+                heroTitle.show();
+                heroSubtitle.show();
+                heroImage.show();
+                heroButton.show();
+                heroMenuText.show();
+                scrollDownText.show();
+                removeEventListener('showAnimationComplete', hero.obj);
+                hero.elem.obj.addEventListener('showAnimationComplete', (event) => {
+                    if (event.target == scrollDownText.obj) {
+                        scrollDownText.highlight();
+                        document.body.classList.remove(util.css.siteClasses.noScroll);
+                    }
+                });
+            }
+        });
+        document.body.classList.add(util.css.siteClasses.noScroll);
+        heroContent.show();
+    }
+}
+
+function handleNavbar() {
+    if (Object.hasOwn(loadedDOMS, templates.templateList.navbarTemplate)) {
+    const navbar = new ui.NavBar(new anim.AnimatedElement(loadedDOMS.navbar[0].shadow.getElementById(util.css.siteElements.navbar),
+                                                            false,
+                                                            new animate.Animation(animate.animationClass.slideInDown,
+                                                                                animate.speedClass.faster,
+                                                                                ),
+                                                            new animate.Animation(animate.animationClass.slideOutUp,
+                                                                                animate.speedClass.faster,
+                                                                                ),
+                                                            ),
+                                    undefined);
+    const navbarTrigger = new anim.AnimationScrollTrigger(navbar.elem,
+                                                            anim.animationType.toggle,
+                                                            new anim.ScrollTriggerElement(hero.elem.obj, anim.anchor.bottom),
+                                                            new anim.ScrollTriggerElement(window),
+                                                            undefined,
+                                                            0,
+                                                            true,
+                                                            true);
+    }
+}
+
 function onLoadComplete() {
     // Reset scroll progress on reload
     util.scrollToTop();
     setTimeout(() => {
-        //#region Hero
-        if (Object.hasOwn(loadedDOMS, 'hero')) {
-            util.log('Hero Detected!');
-            const hero = new ui.Hero(new anim.AnimatedElement(loadedDOMS.hero[0].shadow.getElementById(util.css.siteElements.hero)));
-            const heroButton = new anim.AnimatedElement(loadedDOMS.hero[0].shadow.getElementById('hero-button'),
-                                                        false,
-                                                        new animate.Animation(animate.animationClass.fadeInUp,
-                                                                                animate.speedClass.fast,
-                                                                                1000
-                                                                                ));
-            const heroMenuText = new anim.AnimatedElement(loadedDOMS.hero[0].shadow.getElementById('hero-menu-text'),
-                                                            false,
-                                                            new animate.Animation(animate.animationClass.fadeInUp,
-                                                                                    animate.speedClass.fast,
-                                                                                    1200
-                                                                                    ));
-            const scrollDownText = new anim.AnimatedElement(loadedDOMS.hero[0].shadow.getElementById('scroll-down-text'),
-                                                            false,
-                                                            new animate.Animation(animate.animationClass.fadeInUp,
-                                                                                    animate.speedClass.fast,
-                                                                                    1400
-                                                                                    ),
-                                                            undefined,
-                                                            new animate.Animation(animate.animationClass.bounce,
-                                                                                    animate.speedClass.slow,
-                                                                                    animate.delayClass.delay_4s,
-                                                                                    animate.repeatClass.infinite
-                                                                                    ));
-            const heroTitle = new anim.AnimatedElement(loadedDOMS.hero[0].shadow.getElementById('hero-title'),
-                                                        false,
-                                                        new animate.Animation(animate.animationClass.backInLeft,
-                                                                                animate.speedClass.animated,
-                                                                                ));
-            const heroSubtitle = new anim.AnimatedElement(loadedDOMS.hero[0].shadow.getElementById('hero-subtitle'),
-                                                            false,
-                                                            new animate.Animation(animate.animationClass.backInLeft,
-                                                                                    animate.speedClass.animated,
-                                                                                    500
-                                                                                    ));
-            const heroImage = new anim.AnimatedElement(loadedDOMS.hero[0].shadow.getElementById('hero-image'),
-                                                        false,
-                                                        new animate.Animation(animate.animationClass.backInRight,
-                                                                                animate.speedClass.animated,
-                                                                                200
-                                                                                ));
-            const heroContent = new anim.AnimatedElement(loadedDOMS.hero[0].shadow.getElementById(util.css.siteElements.heroContent),
-                                                            false,
-                                                            new animate.Animation(animate.animationClass.fadeInLeft,
-                                                                                    animate.speedClass.animated,
-                                                                                    ));
-            hero.elem.obj.addEventListener('showAnimationComplete', (event) => {
-                if (event.target == heroContent.obj) {
-                    heroTitle.show();
-                    heroSubtitle.show();
-                    heroImage.show();
-                    heroButton.show();
-                    heroMenuText.show();
-                    scrollDownText.show();
-                    removeEventListener('showAnimationComplete', hero.obj);
-                    hero.elem.obj.addEventListener('showAnimationComplete', (event) => {
-                        if (event.target == scrollDownText.obj) {
-                            scrollDownText.highlight();
-                            document.body.classList.remove(util.css.siteClasses.noScroll);
-                        }
-                    });
-                }
-            });
-            document.body.classList.add(util.css.siteClasses.noScroll);
-            heroContent.show();
+        handleHero();
+        //#region Title Bar
+        titleBar = document.getElementById(pageElements.titleBar);
+        if (titleBar) {
+            util.log('Title Bar Detected!');
+            titleBar = new TitleBar(titleBar);
         }
         //#endregion
+        handleNavbar();
+
 
         if (Object.hasOwn(loadedDOMS, 'sidecard')) {
         const gameSection = new ui.SideCard(new anim.AnimatedElement(loadedDOMS.sidecard[0].shadow.querySelector('.sidecard'),
@@ -125,32 +209,10 @@ function onLoadComplete() {
                                                                               ),
                                                         ));
                                                     }
-        if (Object.hasOwn(loadedDOMS, 'navbar')) {
-        const navbar = new ui.NavBar(new anim.AnimatedElement(loadedDOMS.navbar[0].shadow.getElementById('navbar'),
-                                                              false,
-                                                              new animate.Animation(animate.animationClass.slideInDown,
-                                                                                    animate.speedClass.faster,
-                                                                                    ),
-                                                              new animate.Animation(animate.animationClass.slideOutUp,
-                                                                                    animate.speedClass.faster,
-                                                                                    ),
-                                                              ),
-                                     undefined);
-        const navbarTrigger = new anim.AnimationScrollTrigger(navbar.elem,
-                                                            anim.animationType.toggle,
-                                                            new anim.ScrollTriggerElement(gameSection.elem.obj),
-                                                            new anim.ScrollTriggerElement(window, anim.anchor.bottom),
-                                                            anim.scrollTriggerType.onScrollDown,
-                                                            0,
-                                                            true,
-                                                            true);
-        }
+
         if (Object.hasOwn(loadedDOMS, 'progressbar')) {
-        const progressbar = new ui.ProgressBar(new anim.AnimatedElement(loadedDOMS.progressbar[0].shadow.getElementById('progressbar'),
-                                                                undefined,
-                                                                ),
-                                       );
-        progressbar.start = gameSection.elem.obj.getBoundingClientRect().top - window.innerHeight;
+        const progressbar = new ui.ScrollProgressBar(new anim.AnimatedElement(loadedDOMS.progressbar[0].shadow.getElementById('progressbar')),
+                                                    document.getElementById('start').getBoundingClientRect().top - window.innerHeight);
         }
         if (Object.hasOwn(loadedDOMS, 'horizontalbar')) {
         const bottomBar = new ui.BottomBar(new anim.AnimatedElement(loadedDOMS.horizontalbar[0].shadow.getElementById('bottombar'),
