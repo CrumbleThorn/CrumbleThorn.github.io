34a6abcd855bac17a2e3724c56afd7f5ea4949e6
CrumbleThorn <jctamayo5@alum.up.edu.ph>
2025-07-07 14:36:47 +1000

    Refactored responsive.js

--- js/modules/responsive.js
+++ js/modules/responsive.js
@@ -3,7 +3,7 @@ import * as util from './util.js';
 // TO DO: Implement responsive design
 export function responsiveDesignChecker() {
     if (!util.isLandscape() || util.isLowResolution())
-        util.log("TO DO: Toggle Mobile Mode!");
+        util.log("TO DO: Toggle Mobile Mode!", util.LogType.WARNING);
     else
-        util.log("TO DO: Toggle PC Mode!");
+        util.log("TO DO: Toggle PC Mode!", util.LogType.WARNING);
 }
